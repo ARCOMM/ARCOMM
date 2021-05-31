735999229eb8589dab0674809724d207302ea503
TomBurch <thomas.burch1306@gmail.com>
2021-05-31 19:15:29 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
 
     'aliases' => [
         'App' => Illuminate\Support\Facades\App::class,
-        'Arr' => Illuminate\Support\Arr::Class,
+        'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
